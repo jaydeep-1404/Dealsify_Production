6dc2af42694f1bcdc15ff01a0c039169b2896745
jaydeepmakwana <jaydeepmakwana575@gmail.com>
2024-10-14 10:38:59 +0530

    login screen

--- lib/src/screens/splash/splash.dart
+++ lib/src/screens/splash/splash.dart
@@ -1,20 +1,45 @@
 
+import 'package:dealsify_production/src/screens/dashboard/dashboard.dart';
 import 'package:dealsify_production/src/screens/login/login.dart';
 import 'package:flutter/material.dart';
+
+import '../../../core/services/local_storage.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  _SplashScreenState createState() => _SplashScreenState();
+  State<SplashScreen> createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  var userInfo = LocalDataModel();
+
   @override
   void initState() {
     super.initState();
+    checkToken();
+  }
 
-    Future.delayed(const Duration(seconds: 2), () {
+  Future<void> checkToken() async {
+    await Future.delayed(const Duration(seconds: 2));
+    userInfo = (await pref.get())!;
+
+    if (userInfo.access_token != null && userInfo.access_token.isNotEmpty) {
+      Navigator.pushReplacement(
+        context,
+        PageRouteBuilder(
+          transitionDuration: const Duration(seconds: 1),
+          pageBuilder: (context, animation, secondaryAnimation) => const DashboardScreen(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+        ),
+      );
+    } else {
       Navigator.pushReplacement(
         context,
         PageRouteBuilder(
@@ -28,8 +53,10 @@ class _SplashScreenState extends State<SplashScreen> {
           },
         ),
       );
-    });
+    }
   }
+
+  Future<void> getDataFromLocal() async => userInfo = (await pref.get())!;
 
   @override
   Widget build(BuildContext context) {
